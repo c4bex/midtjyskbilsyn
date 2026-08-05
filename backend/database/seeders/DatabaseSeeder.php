c381758b351b4444832adc948caa81ca4837e386
c4bex <havn90@gmail.com>
2026-08-05 16:24:04 +0200

    Make Laravel demo seeder repeatable

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -16,9 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
+        User::firstOrCreate(['email' => 'test@example.com'], [
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'password' => bcrypt('password'),
         ]);
 
         foreach ([
